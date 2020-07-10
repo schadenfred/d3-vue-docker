f4c10e721d82c5864199e1591378256bf27babff
schadenfred <fred.schoeneman@gmail.com>
2020-07-10 14:23:36 -0700

    wiring d3, vue, dockertogether

---
+++ README.md
@@ -0,0 +1,21 @@
+# d3-vue-example
+
+Repository supporting the 'D3.js and Vue.js' blog post on Medium. Link [here](https://medium.com/@andre.devries/d3-js-and-vue-js-7a6a721eb79f)
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <PackChart :tweetData=loadData />
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+import HelloWorld from "./components/HelloWorld.vue";
+import PackChart from "./components/Chart.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    PackChart
+  },
+  data: function() {
+    return {
+      loadData: []
+    };
+  },
+  mounted() {
+    console.log("App loaded");
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      let data = await d3.json("./tweets.json");
+      this.loadData = data;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
